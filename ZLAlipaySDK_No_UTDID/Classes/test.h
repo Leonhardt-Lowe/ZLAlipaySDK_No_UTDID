c34f7e43d82c0a5896c50c6106d973094ebc3d6e
zL <344717759@qq.com>
2020-07-08 16:31:35 +0800

    test

---
+++ ZLAlipaySDK_No_UTDID/Classes/test.h
@@ -0,0 +1,16 @@
+//
+//  test.h
+//  ZLAlipaySDK_No_UTDID
+//
+//  Created by Lowe on 2020/7/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZLAlipaySDK_No_UTDID/Classes/test.m
@@ -0,0 +1,12 @@
+//
+//  test.m
+//  ZLAlipaySDK_No_UTDID
+//
+//  Created by Lowe on 2020/7/8.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
